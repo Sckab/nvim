464ee11918d3a5473f233163322e5261754041f9
Sckab <giulianodeamicis2009@gmail.com>
2025-10-16 15:30:13 +0200

    changed completition engine, from cmp-nvim to blink.cmp, now it's cooler and faster!

--- lua/plugins/language/auto-completition.lua
+++ lua/plugins/language/auto-completition.lua
@@ -1,138 +1,62 @@
--- return {
---     {
---         "L3MON4D3/LuaSnip",
---         dependencies = { "rafamadriz/friendly-snippets" },
---         config = function()
---             require("luasnip.loaders.from_vscode").lazy_load()
---         end,
---     },
---     {
---         'saghen/blink.cmp',
---         dependencies = { 'rafamadriz/friendly-snippets' },
---
---         version = '1.*',
---
---         build = 'cargo build --release',
---
---         ---@module 'blink.cmp'
---         ---@type blink.cmp.Config
---         opts = {
---             keymap = { preset = 'super-tab' },
---
---             signature = { enabled = true },
---
---             appearance = {
---                 nerd_font_variant = 'normal'
---             },
---
---             completion = { documentation = { auto_show = true } },
---
---             sources = {
---                 default = { 'lsp', 'path', 'snippets', 'buffer' },
---             },
---
---             fuzzy = { implementation = "prefer_rust_with_warning" },
---         },
---         opts_extend = { "sources.default" },
---     },
---     {
---         "rafamadriz/friendly-snippets",
---         lazy = true,
---     },
--- }
-
 return {
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = { "rafamadriz/friendly-snippets" },
-        config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/cmp-nvim-lua",
-            "hrsh7th/cmp-nvim-lsp-signature-help",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-            "onsails/lspkind.nvim",
-            "brenoprata10/nvim-highlight-colors",
-        },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            local lspkind = require("lspkind")
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{
+		"rafamadriz/friendly-snippets",
+		lazy = true,
+	},
+	{
+		"saghen/blink.cmp",
+		dependencies = { "rafamadriz/friendly-snippets", "L3MON4D3/LuaSnip", version = "v2.*" },
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
+		version = "1.*",
 
-                window = {
-                    completion = {
-                        border = "none",
-                    },
-                    documentation = {
-                        border = "none",
-                    },
-                },
+		build = "cargo build --release",
 
-                experimental = {
-                    ghost_text = true,
-                },
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = { preset = "super-tab" },
 
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "symbol_text",
-                        maxwidth = 50,
-                        ellipsis_char = "...",
-                    }),
-                },
+			snippets = { preset = "luasnip" },
 
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-Space>"] = cmp.mapping.complete(),
+			signature = { enabled = true },
 
-                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-j>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+			appearance = {
+				nerd_font_variant = "normal",
+			},
 
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            local entry = cmp.get_selected_entry()
-                            if entry then
-                                cmp.confirm({ select = true })
-                            else
-                                cmp.select_next_item()
-                            end
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                }),
+			cmdline = {
+				keymap = { preset = "inherit" },
+				completion = { menu = { auto_show = true } },
+			},
 
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "buffer" },
-                    { name = "path" },
-                    { name = "nvim_lua" },
-                    { name = "cmdline" },
-                    { name = "nvim_lsp_signature_help" },
-                    { name = "spell" },
-                    { name = "highlight_colors" },
-                }),
-            })
-        end,
-    },
+			completion = {
+				menu = {
+					draw = {
+						columns = {
+							{ "kind_icon", "label" },
+							{ "kind" },
+						},
+					},
+				},
+
+				documentation = {
+					auto_show = true,
+				},
+			},
+
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
 }
