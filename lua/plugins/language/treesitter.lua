8a73d2796fa335db304bda6f065e0d5d72beb62f
Sckab <giulianodeamicis2009@gmail.com>
2025-10-17 14:17:54 +0200

    added textobjects and barbecue

--- lua/plugins/language/treesitter.lua
+++ lua/plugins/language/treesitter.lua
@@ -1,30 +1,59 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"astro",
-				"html",
-				"css",
-				"javascript",
-				"typescript",
-				"bash",
-				"c_sharp",
-				"toml",
-				"json",
-				"xml",
-				"yaml",
-				"markdown",
-				"python",
-				"go",
-				"gomod",
-				"gosum",
-				"gowork",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"astro",
+					"html",
+					"css",
+					"javascript",
+					"typescript",
+					"bash",
+					"c_sharp",
+					"toml",
+					"json",
+					"xml",
+					"yaml",
+					"markdown",
+					"python",
+					"go",
+					"gomod",
+					"gosum",
+					"gowork",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+				textobjects = {
+					select = {
+						enable = true,
+
+						lookahead = true,
+
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+							["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+						},
+
+						selection_modes = {
+							["@parameter.outer"] = "v",
+							["@function.outer"] = "V",
+							["@class.outer"] = "<c-v>",
+						},
+
+						include_surrounding_whitespace = true,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+	},
 }
