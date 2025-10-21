f4b22d6ead5e7685aa9a15565151b1696faea7ed
Sckab <giulianodeamicis2009@gmail.com>
2025-10-21 21:15:22 +0200

    removed the undefined global vim error

--- lua/plugins/language/mason.lua
+++ lua/plugins/language/mason.lua
@@ -1,28 +1,42 @@
 return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = {
-		{ "williamboman/mason.nvim", opts = {} },
-		"neovim/nvim-lspconfig",
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			{ "williamboman/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"astro",
+					"html",
+					"cssls",
+					"ts_ls",
+					"tailwindcss",
+					"csharp_ls",
+					"bashls",
+					"taplo",
+					"jsonls",
+					"lemminx",
+					"marksman",
+					"pyright",
+					"gopls",
+				},
+			})
+
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = {
+								"vim",
+							},
+						},
+					},
+				},
+			})
+		end,
 	},
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"astro",
-				"html",
-				"cssls",
-				"ts_ls",
-				"tailwindcss",
-				"csharp_ls",
-				"bashls",
-				"taplo",
-				"jsonls",
-				"lemminx",
-				"marksman",
-				"pyright",
-				"gopls",
-			},
-		})
-	end,
 }
