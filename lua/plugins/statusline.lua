724dd5ad04271acc397690cd3f83c7274d641308
Sckab <giulianodeamicis2009@gmail.com>
2025-11-06 19:52:01 +0100

    changed the lualine config, adding a new winbar

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -14,22 +14,51 @@ return {
 			options = {
 				theme = "auto",
 				icons_enabled = true,
-				section_separators = { left = "", right = "" },
-				component_separators = { left = "│", right = "│" },
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
 				globalstatus = true,
 				disabled_filetypes = {
 					statusline = { "alpha", "snacks_dashboard" },
-					winbar = {},
+					winbar = { "snacks_dashboard", "neo-tree" },
 				},
 			},
+
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_c = { "encoding" },
+				lualine_x = {
+					{
+						"fileformat",
+						symbols = {
+							unix = " Unix",
+							dos = " DOS",
+							mac = " Mac",
+						},
+					},
+				},
 				lualine_y = { "progress", "location" },
 				lualine_z = { clock },
 			},
+
+			winbar = {
+				lualine_a = { "filename" },
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = { "filetype" },
+				lualine_z = { "lsp_status" },
+			},
+
+			inactive_winbar = {
+				lualine_a = { "filename" },
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = { "filetype" },
+				lualine_z = { "lsp_status" },
+			},
+
 			extensions = {
 				"neo-tree",
 				"aerial",
