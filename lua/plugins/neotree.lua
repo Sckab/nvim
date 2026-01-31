d478e3a956fc76628f382145eddcfcc7e10f3b87
Sckab <giulianodeamicis2009@gmail.com>
2026-02-01 00:50:26 +0100

    update neotree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -77,40 +77,6 @@ return {
 			},
 		},
 
-		source_selector = {
-			winbar = true,
-			statusline = true,
-			show_scrolled_off_parent_node = false,
-			sources = {
-				{
-					source = "filesystem",
-					display_name = "󰉓 Files",
-				},
-				{
-					source = "buffers",
-					display_name = "󰈚 Buffers",
-				},
-				{
-					source = "git_status",
-					display_name = "󰊢 Git",
-				},
-			},
-			content_layout = "center",
-			tabs_layout = "center",
-			truncation_character = "…",
-			tabs_min_width = nil,
-			tabs_max_width = nil,
-			padding = 0,
-			separator = { left = "▏", right = "▕" },
-			separator_active = nil,
-			show_separator_on_edge = false,
-			highlight_tab = "NeoTreeTabInactive",
-			highlight_tab_active = "NeoTreeTabActive",
-			highlight_background = "NeoTreeTabInactive",
-			highlight_separator = "NeoTreeTabSeparatorInactive",
-			highlight_separator_active = "NeoTreeTabSeparatorActive",
-		},
-
 		window = {
 			position = "left",
 			width = 31,
